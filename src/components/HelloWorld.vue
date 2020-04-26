7284148e17d891df6f85dccece2e5a53f3b12aec
eryndrem <cats.7777@mail.ru>
2020-04-26 22:31:42 +0300

    Проект создан и проконфигурирован, настроен eslinter.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="hello__message">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -37,26 +37,31 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+/**
+ * @class HelloWorld
+ * @property {string} msg - текст сообщения
+ * @method {void} someMethod - какой-то метод
+ */
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+    @Prop() private msg!: string;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+      list-style-type: none;
+      padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+      display: inline-block;
+      margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
