6ba6b9c15f9cc7619624624ed5b02c8a05c5d2be
eryndrem <cats.7777@mail.ru>
2020-04-28 00:25:06 +0300

    Добавлен компонент Alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,52 @@
+<template>
+    <transition name="fade">
+        <div v-show="visible"
+             role="alert"
+             :class="{
+                'alert--primary': color === Colors.PRIMARY,
+                'alert--secondary': color === Colors.SECONDARY,
+                'alert--success': color === Colors.SUCCESS,
+                'alert--warning': color === Colors.WARNING,
+                'alert--danger': color === Colors.DANGER,
+                'alert--info': color === Colors.INFO,
+             }"
+             class="alert">
+
+            <btn class="alert__close"
+                 @click="visible = false">
+                <img class="alert__image"
+                     src="../assets/icons/x.svg" alt="Закрыть">
+            </btn>
+
+            <h3 class="alert__header"
+                v-text="title"></h3>
+            <p class="alert__body"
+               v-text="content"></p>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Colors } from '@/common/constants';
+
+/**
+ * @class Alert
+ * @property {Colors} color - цвет уведомления
+ * @property {string} title - название уведомления
+ * @property {string} content - тело уведомления
+ * @property {boolean} visible - видимость уведомления
+ */
+@Component
+export default class Alert extends Vue {
+    Colors = Colors;
+
+    @Prop() color?: Colors;
+
+    @Prop(Boolean) visible!: boolean;
+
+    @Prop({ required: true }) title!: string;
+
+    @Prop({ required: true }) content!: string;
+}
+</script>
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -8,6 +8,57 @@
         <btn :color="Colors.DANGER">Danger</btn>
         <btn :color="Colors.INFO">Info</btn>
 
+         <!-- Alert -->
+        <alert visible
+               :color="Colors.PRIMARY"
+               title="Primary"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+        <alert visible
+               :color="Colors.SECONDARY"
+               title="Secondary"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+        <alert visible
+               :color="Colors.SUCCESS"
+               title="Success"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+        <alert visible
+               :color="Colors.WARNING"
+               title="Warning"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+        <alert visible
+               :color="Colors.DANGER"
+               title="Danger"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+        <alert visible
+               :color="Colors.INFO"
+               title="Info"
+               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce id fermentum quam. Proin sagittis, nibh id hendrerit
+                imperdiet, elit sapien laoreet elit.">
+
+        </alert>
+
+
         <!-- Badge -->
         <badge :color="Colors.PRIMARY">Primary</badge>
         <badge :color="Colors.SECONDARY">Secondary</badge>
@@ -25,11 +76,13 @@ import Component from 'vue-class-component';
 
 import { Colors } from '@/common/constants';
 import Badge from '@/components/Badge.vue';
+import Alert from '@/components/Alert.vue';
 
 
 @Component({
     components: {
         badge: Badge,
+        alert: Alert,
     },
 })
 export default class Demo extends Vue {
